146a4a051948fbba19d67152773e331e45e5508a
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-06-05 17:38:48 +0200

    [feat] Milestone 1

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Main = () => {
+
+  const [politicians, setPoliticians] = useState([]);
+
+  useEffect(() => {
+    fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians")
+      .then(res => res.json())
+      .then(data => setPoliticians(data))
+      .catch(err => console.error(error))
+  }, []);
+
+  console.log(politicians);
+
   return (
-    <div>Main</div>
+    <div>
+      <h1 className='text-danger m-4'>Lista dei politici</h1>
+      <div className='container'>
+        {politicians.map((p) => {
+          <div className='card' key={p.id}>
+            <img src={p.image} alt={p.name} />
+            <h2>{p.name}</h2>
+            <p><strong>Posizione:</strong>{p.position}</p>
+            <p>{p.biography}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
